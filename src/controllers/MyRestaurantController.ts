31ee2f1df724dce6d8ef344d6a00b6ade684a39d
Chantal <chantal@Chantals-MacBook-Air.local>
2024-03-02 15:44:41 -0500

    validation for createMyRestaurant added

--- src/controllers/MyRestaurantController.ts
+++ src/controllers/MyRestaurantController.ts
@@ -7,7 +7,7 @@ const createMyRestaurant = async (req: Request, res: Response) => {
   try {
     //to check if user has an existing restaurant in the db. 
     //user can only add one restaurant.
-    const existingRestaurant = await Restaurant.find({ user: req.userId })
+    const existingRestaurant = await Restaurant.findOne({ user: req.userId })
 
     //if user already has a restaurant
     if (existingRestaurant) {
@@ -18,9 +18,10 @@ const createMyRestaurant = async (req: Request, res: Response) => {
 
     //to create data uri string that represents the image we got in the req//cloudinary
     const image = req.file as Express.Multer.File
+
     //we convert the img to a base 64 string and we pass it to a datauri string:
     const base64Image = Buffer.from(image.buffer).toString("base64")
-    const dataURI = `data:${image.mimetype};base64,${base64Image}`
+    const dataURI = `data:${image.mimetype};base64,${base64Image}`;
 
     //we upload the img to cloudinary:
     const uploadResponse = await cloudinary.v2.uploader.upload(dataURI)//img url in the uploadResonse
--- src/routes/MyRestaurantRoute.ts
+++ src/routes/MyRestaurantRoute.ts
@@ -3,6 +3,8 @@
 import express from "express"
 import multer from "multer"
 import MyRestaurantController from "../controllers/MyRestaurantController"
+import { jwtCheck, jwtParse } from "../middleware/auth"
+import { validateMyRestaurantRequest } from "../middleware/validation"
 
 const router = express.Router()
 
@@ -12,13 +14,16 @@ const upload = multer({
   storage: storage,
   limits: {
     fileSize: 5 * 1024 * 1024, //5mb
-  }
+  },
 })
 
 // /api/my/restaurant
 router.post(
   "/",
   upload.single("imageFile"),
+  validateMyRestaurantRequest,
+  jwtCheck, //to ensure we get a valid token in the request
+  jwtParse, // it pulls the current logged in user info out of the token and passes it on to the request
   MyRestaurantController.createMyRestaurant
 )
 
